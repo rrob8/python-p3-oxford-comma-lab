d3db122d26432739dca2c92dd055c1b7158e6531
Robert Bubnik <rbubnik@gmail.com>
2024-04-06 14:40:21 -0400

    passed all tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,20 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+
+    if len(items) > 1 and len(items) < 3:
+        return ' and '.join(items)
+
+    if len(items) > 2:
+        phrase = []
+        last = ', and ' + (items.pop(-1))
+        ' '.join(last)
+
+        oxford = ', '.join(items)
+
+        phrase.append(oxford)
+        phrase.append(last)
+
+        return ''.join(phrase)
+
+    
